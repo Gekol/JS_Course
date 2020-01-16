66b26c85562f3b526f874db5daf63f6f68cf47eb
Georgyi Sokolovskyi <george.sokolovsky2001@gmail.com>
2020-01-16 11:05:16 +0200

    Logs removed

--- lesson6/app/abbreviation.js
+++ lesson6/app/abbreviation.js
@@ -1,12 +1,10 @@
 function makeAbbreviation(string) {
     let dashWords = string.split("-");
-    console.log(dashWords);
     let words = dashWords.reduce((accumulator, elem) => {
         let words = elem.split(" ");
         accumulator = accumulator.concat(words);
         return accumulator
     }, []);
-    console.log(words);
     return words.reduce((accumulator, elem) => accumulator + elem[0].toUpperCase(), "");
 }
 
